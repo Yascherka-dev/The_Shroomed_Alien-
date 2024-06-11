8d09d62116159989855da643805bbbc5162929f6
Yasmina CHERKAOUI <yasmina.cerkaoui@mail-esd.com>
2024-06-11 17:02:04 +0200

    ajout d'assets + effet static

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ function setup() {
     tunnel.x = 0;
     tunnel.y = 689;
 
+
 tunnel2 = new Sprite();
 	tunnel2.width = 7;
 	tunnel2.height = 465;
@@ -76,7 +77,80 @@ tunnel2 = new Sprite();
     tunnel9.y = 95;
 
 player1 = new Sprite();
+player1.x = 29;
+player1.y = 645;
+world.gravity.y = 10;
+
+esc1 = new Sprite();
+esc1.img = "./img/esc1.png";
+ esc1.x = 181;
+esc1.y = 600 ;
+esc1.scale = 0.5;
+
+esc2 = new Sprite();
+esc2.img = "./img/esc2.png";
+ esc2.x = 96;
+esc2.y = 548 ;
+esc2.scale = 0.5;
+
+esc3= new Sprite();
+esc3.img = "./img/esc1.png";
+ esc3.x = 181;
+esc3.y = 300;
+esc3.scale = 0.5;
+
+esc4 = new Sprite();
+esc4.img = "./img/esc2.png";
+ esc4.x = 96;
+esc4y = 200 ;
+esc4.scale = 0.5;
+
+esc5 = new Sprite();
+esc5.img = "./img/esc1.png";
+ esc5.x = 181;
+esc5.y = 500 ;
+esc5.scale = 0.5;
+
+esc6 = new Sprite();
+esc6.img = "./img/esc2.png";
+ esc6.x = 96;
+esc6.y = 441;
+esc6.scale = 0.5;
+
+
+esc7 = new Sprite();
+esc7.img = "./img/esc1.png";
+ esc7.x = 181;
+esc7.y = 411 ;
+esc7.scale = 0.5;
+
+
+
+tunnel.collider = 'static';
+tunnel2.collider = 'static';
+tunnel3.collider = 'static';
+tunnel4.collider = 'static';
+tunnel5.collider = 'static';
+tunnel6.collider = 'static';
+tunnel7.collider = 'static';
+tunnel8.collider = 'static';
+tunnel9.collider = 'static';
+champi3.collider ='static';
+esc1.collider = 'static';
+esc2.collider = 'static';
+esc3.collider = 'static';
+esc4.collider = 'static';
+esc5.collider = 'static';
+esc6.collider = 'static';
+esc7.collider = 'static';
+
+
+champi1.collider ='kinematic';
+champi2.collider ='kinematic';
+
 }
+
+
 
 function draw() {
   // Votre code de dessin ici
@@ -87,7 +161,9 @@ function draw() {
     else player1.vel.x = 0;
     if (kb.pressing('space') && player1.vel.y <= 0.2) {
     player1.vel.y = -3;
+
 }
+
 }
 
 
